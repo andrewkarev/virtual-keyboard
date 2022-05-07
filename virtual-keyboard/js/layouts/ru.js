2e309d7f3528186fbaba21e5104a09ccf8dba960
andrewkarev <an.carev4@gmail.com>
2022-05-07 15:16:19 +0300

    feat: add russian layout keys description

---
+++ virtual-keyboard/js/layouts/ru.js
@@ -0,0 +1,324 @@
+const ru = [
+  {
+    symbol: 'ё',
+    shift: 'Ё',
+    code: 'Backquote',
+  },
+  {
+    symbol: '1',
+    shift: '!',
+    code: 'Digit1',
+  },
+  {
+    symbol: '2',
+    shift: '"',
+    code: 'Digit2',
+  },
+  {
+    symbol: '3',
+    shift: '№',
+    code: 'Digit3',
+  },
+  {
+    symbol: '4',
+    shift: ';',
+    code: 'Digit4',
+  },
+  {
+    symbol: '5',
+    shift: '%',
+    code: 'Digit5',
+  },
+  {
+    symbol: '6',
+    shift: ':',
+    code: 'Digit6',
+  },
+  {
+    symbol: '7',
+    shift: '?',
+    code: 'Digit7',
+  },
+  {
+    symbol: '8',
+    shift: '*',
+    code: 'Digit8',
+  },
+  {
+    symbol: '9',
+    shift: '(',
+    code: 'Digit9',
+  },
+  {
+    symbol: '0',
+    shift: ')',
+    code: 'Digit0',
+  },
+  {
+    symbol: '-',
+    shift: '_',
+    code: 'Minus',
+  },
+  {
+    symbol: '=',
+    shift: '+',
+    code: 'Equal',
+  },
+  {
+    symbol: 'Del',
+    shift: null,
+    code: 'Delete',
+  },
+  {
+    symbol: '&#8633;',
+    shift: null,
+    code: 'Tab',
+  },
+  {
+    symbol: 'й',
+    shift: 'Й',
+    code: 'KeyQ',
+  },
+  {
+    symbol: 'ц',
+    shift: 'Ц',
+    code: 'KeyW',
+  },
+  {
+    symbol: 'у',
+    shift: 'У',
+    code: 'KeyE',
+  },
+  {
+    symbol: 'к',
+    shift: 'К',
+    code: 'KeyR',
+  },
+  {
+    symbol: 'е',
+    shift: 'Е',
+    code: 'KeyT',
+  },
+  {
+    symbol: 'н',
+    shift: 'Н',
+    code: 'KeyY',
+  },
+  {
+    symbol: 'г',
+    shift: 'Г',
+    code: 'KeyU',
+  },
+  {
+    symbol: 'ш',
+    shift: 'Ш',
+    code: 'KeyI',
+  },
+  {
+    symbol: 'щ',
+    shift: 'Щ',
+    code: 'KeyO',
+  },
+  {
+    symbol: 'з',
+    shift: 'З',
+    code: 'KeyP',
+  },
+  {
+    symbol: 'х',
+    shift: 'Х',
+    code: 'BracketLeft',
+  },
+  {
+    symbol: 'ъ',
+    shift: 'Ъ',
+    code: 'BracketRight',
+  },
+  {
+    symbol: '\\',
+    shift: '/',
+    code: 'Backslash',
+  },
+  {
+    symbol: '&#9003;',
+    shift: null,
+    code: 'Backspace',
+  },
+  {
+    symbol: '&#8682;',
+    shift: null,
+    code: 'CapsLock',
+  },
+  {
+    symbol: 'ф',
+    shift: 'Ф',
+    code: 'KeyA',
+  },
+  {
+    symbol: 'ы',
+    shift: 'Ы',
+    code: 'KeyS',
+  },
+  {
+    symbol: 'в',
+    shift: 'В',
+    code: 'KeyD',
+  },
+  {
+    symbol: 'а',
+    shift: 'А',
+    code: 'KeyF',
+  },
+  {
+    symbol: 'п',
+    shift: 'П',
+    code: 'KeyG',
+  },
+  {
+    symbol: 'р',
+    shift: 'Р',
+    code: 'KeyH',
+  },
+  {
+    symbol: 'о',
+    shift: 'О',
+    code: 'KeyJ',
+  },
+  {
+    symbol: 'л',
+    shift: 'Л',
+    code: 'KeyK',
+  },
+  {
+    symbol: 'д',
+    shift: 'Д',
+    code: 'KeyL',
+  },
+  {
+    symbol: 'ж',
+    shift: 'Ж',
+    code: 'Semicolon',
+  },
+  {
+    symbol: 'э',
+    shift: 'Э',
+    code: 'Quote',
+  },
+  {
+    symbol: '&#8617;',
+    shift: null,
+    code: 'Enter',
+  },
+  {
+    symbol: '&#8679;',
+    shift: null,
+    code: 'ShiftLeft',
+  },
+  {
+    symbol: 'я',
+    shift: 'Я',
+    code: 'KeyZ',
+  },
+  {
+    symbol: 'ч',
+    shift: 'Ч',
+    code: 'KeyX',
+  },
+  {
+    symbol: 'с',
+    shift: 'С',
+    code: 'KeyC',
+  },
+  {
+    symbol: 'м',
+    shift: 'М',
+    code: 'KeyV',
+  },
+  {
+    symbol: 'и',
+    shift: 'И',
+    code: 'KeyB',
+  },
+  {
+    symbol: 'т',
+    shift: 'Т',
+    code: 'KeyN',
+  },
+  {
+    symbol: 'ь',
+    shift: 'Ь',
+    code: 'KeyM',
+  },
+  {
+    symbol: 'б',
+    shift: 'Б',
+    code: 'Comma',
+  },
+  {
+    symbol: 'ю',
+    shift: 'Ю',
+    code: 'Period',
+  },
+  {
+    symbol: '.',
+    shift: ',',
+    code: 'Slash',
+  },
+  {
+    symbol: '&#9652;',
+    shift: null,
+    code: 'ArrowUp',
+  },
+  {
+    symbol: '&#8679;',
+    shift: null,
+    code: 'ShiftRight',
+  },
+  {
+    symbol: 'control',
+    shift: null,
+    code: 'ControlLeft',
+  },
+  {
+    symbol: '&#8862;',
+    shift: null,
+    code: 'MetaLeft',
+  },
+  {
+    symbol: 'alt',
+    shift: null,
+    code: 'AltLeft',
+  },
+  {
+    symbol: ' ',
+    shift: null,
+    code: 'Space',
+  },
+  {
+    symbol: 'alt',
+    shift: null,
+    code: 'AltRight',
+  },
+  {
+    symbol: '&#9666;',
+    shift: null,
+    code: 'ArrowLeft',
+  },
+  {
+    symbol: '&#9662;',
+    shift: null,
+    code: 'ArrowDown',
+  },
+  {
+    symbol: '&#9656;',
+    shift: null,
+    code: 'ArrowRight',
+  },
+  {
+    symbol: 'control',
+    shift: null,
+    code: 'ControlRight',
+  },
+];
+
+export default ru;
